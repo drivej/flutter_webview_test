1c0b04c435c6ec1aa5409b45b3c88f2e70b80270
drivej <drivej@hotmail.com>
2024-01-31 12:51:24 -0500

    x

---
+++ lib/webview.dart
@@ -0,0 +1,26 @@
+// import 'package:flutter/material.dart';
+// // import 'package:webview_flutter/webview_flutter.dart';
+
+// class WebViewPage extends StatefulWidget {
+//   final String url;
+
+//   WebViewPage({required this.url});
+
+//   @override
+//   _WebViewPageState createState() => _WebViewPageState();
+// }
+
+// class _WebViewPageState extends State<WebViewPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Webview"),
+//       ),
+//       body: WebView(
+//         initialUrl: widget.url,
+//         javascriptMode: JavascriptMode.unrestricted,
+//       ),
+//     );
+//   }
+// }
